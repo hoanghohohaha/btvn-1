27704e2526a487e7231e240079df445724e7615b
hoanghohohaha <56223669+hoanghohohaha@users.noreply.github.com>
2019-11-10 17:37:39 +0700

    Add files via upload

--- gthieu.js
+++ gthieu.js
@@ -1,6 +1,59 @@
+    document.getElementById('box1info').style.display='none';
+    document.getElementById('box2info').style.display='none';
+    document.getElementById('box3info').style.display='none';
+    document.getElementById('box4info').style.display='none';
 setTimeout(() => {
     document.getElementById('mainclass').style.display='flex';
     document.getElementById('boxcontain').style.display='flex';
     document.getElementById('navbar').style.display='flex';
     document.getElementById('loadingcontain').style.display='none';
 },2000);
+function back(){
+    document.getElementById('mainclass').style.display='flex';
+    document.getElementById('boxcontain').style.display='flex';
+    document.getElementById('navbar').style.display='flex';
+    document.getElementById('box1info').style.display='none';
+    document.getElementById('box2info').style.display='none';
+    document.getElementById('box3info').style.display='none';
+    document.getElementById('box4info').style.display='none';
+}
+function box1(){
+    document.getElementById('loadingcontain').style.display="flex";
+    document.getElementById('mainclass').style.display='none';
+    document.getElementById('boxcontain').style.display='none';
+    document.getElementById('navbar').style.display='none';
+    setTimeout(() => {
+        document.getElementById('box1info').style.display='flex';
+        document.getElementById('loadingcontain').style.display='none';
+    },1000);
+}
+function box3(){
+    document.getElementById('loadingcontain').style.display="flex";
+    document.getElementById('mainclass').style.display='none';
+    document.getElementById('boxcontain').style.display='none';
+    document.getElementById('navbar').style.display='none';
+    setTimeout(() => {
+        document.getElementById('box3info').style.display='flex';
+        document.getElementById('loadingcontain').style.display='none';
+    },1000);
+}
+function box2(){
+    document.getElementById('loadingcontain').style.display="flex";
+    document.getElementById('mainclass').style.display='none';
+    document.getElementById('boxcontain').style.display='none';
+    document.getElementById('navbar').style.display='none';
+    setTimeout(() => {
+        document.getElementById('box2info').style.display='flex';
+        document.getElementById('loadingcontain').style.display='none';
+    },1000);
+}
+function box4(){
+    document.getElementById('loadingcontain').style.display="flex";
+    document.getElementById('mainclass').style.display='none';
+    document.getElementById('boxcontain').style.display='none';
+    document.getElementById('navbar').style.display='none';
+    setTimeout(() => {
+        document.getElementById('box4info').style.display='flex';
+        document.getElementById('loadingcontain').style.display='none';
+    },1000);
+}
