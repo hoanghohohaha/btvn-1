9f1af0d1ab619596820ac755e306f91f4ea13944
hoanghohohaha <56223669+hoanghohohaha@users.noreply.github.com>
2019-11-10 14:59:37 +0700

    Add files via upload

---
+++ gthieu.js
@@ -0,0 +1,6 @@
+setTimeout(() => {
+    document.getElementById('mainclass').style.display='flex';
+    document.getElementById('boxcontain').style.display='flex';
+    document.getElementById('navbar').style.display='flex';
+    document.getElementById('loadingcontain').style.display='none';
+},2000);
